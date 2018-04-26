c995e18464bc69bf8f083e9d2f9b4ee2b8c2d0bb
fionamiaojing <fionamiaojing@gmail.com>
2018-04-26 16:18:05 -0700

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //input: string
+  //output: nothing
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  //if set._storage contains the key named item
+  //if true: return true
+  
+  return (item in this._storage);
 };
 
 setPrototype.remove = function(item) {
+  if (item in this._storage) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add - O(1)
+  contains - O(1)
+  remove - O(1)
  */
