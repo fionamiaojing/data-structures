f7d63b2530a5b7668ad1325212d0c72c124b5b1c
fionamiaojing <fionamiaojing@gmail.com>
2018-04-26 20:59:57 -0700

    Initiate functions in BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,18 @@
 var BinarySearchTree = function(value) {
+  
 };
 
+BinarySearchTree.prototype.insert = function(){
+  
+};
+
+BinarySearchTree.prototype.contains = function(){
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(){
+  
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
