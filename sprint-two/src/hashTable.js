0529ab1b47ba539610ceae8edde2e8a263048871
fionamiaojing <fionamiaojing@gmail.com>
2018-04-26 20:51:51 -0700

    Partially complete hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,4 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +6,25 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //use limitedArray.set 
+  //i: index, v
+  //o: nothing
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, v);
+  } else {
+    //this._storage.set(index,Object.assign(this._storage.get(index),{k,v}));
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
